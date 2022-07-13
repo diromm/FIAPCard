5cf4b4c9659e14264cd79174b811dd6fbff88082
Diogo Martins <diogo.romm@gmail.com>
2022-07-12 22:03:13 -0300

    atualiza projeto

--- app/src/test/java/com/fiap/fiapcard/FiapCardApplicationTests.java
+++ app/src/test/java/com/fiap/fiapcard/FiapCardApplicationTests.java
@@ -1,13 +1,13 @@
-package com.fiap.fiapcard;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FiapCardApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.fiap.fiapcard;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FiapCardApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
